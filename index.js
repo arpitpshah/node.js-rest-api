47876f5c13fc4be98cfc1c22fea214d477ee0eb3
arpitpshah <arpitshah358@gmail.com>
2021-05-03 19:31:54 +0530

    Setup app for Heroku

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var express=require("express");
-var bodyParser=require('body-parser');
-var app = express();
-var authenticateRoute=require('./api/controllers/authenticate');
-var registerRoute=require('./api/controllers/register');
-var getDataRoute=require('./api/controllers/getData');
+const express=require("express");
+const bodyParser=require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000
+
+const authenticateRoute=require('./api/controllers/authenticate');
+const registerRoute=require('./api/controllers/register');
+const getDataRoute=require('./api/controllers/getData');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-/* route to handle login and registration */
+
 app.post('/api/register',registerRoute.register);
 app.post('/api/authenticate',authenticateRoute.authenticate);
 app.get('/api/getData/:fname/:lname/:employeeID',getDataRoute.getUserData);
 app.get('/api/getData/sort',getDataRoute.sortUserData);
-app.listen(3000);
+
+app.listen(port);
